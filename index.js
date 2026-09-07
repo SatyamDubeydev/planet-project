7c5dc8caf918984610dcf1f519307e71030f95a9
satyam dubey <satyamdubey@satyams-MacBook-Air.local>
2026-09-07 17:18:33 +0530

    added a isHabitable planet function to check lives

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { parse } = require('csv-parse');
 const fs = require("fs");
-const results = [];
+const HabitablePlanet = [];
+//this function will check if the planet is habitable or not. If the planet is habitable then it will return true otherwise it will return false.
+const isHabitablePlanet = (planet) => {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        }
 
 //first we will read the csv file and then we will parse it and then we will push the data into the results array and then we will log the results array to the console.
 fs.createReadStream("kepler_data.csv")
@@ -11,9 +15,11 @@ fs.createReadStream("kepler_data.csv")
     }
 ))
 .on("data",(data)=>{
-    results.push(data);
+    if(isHabitablePlanet(data)){
+    HabitablePlanet.push(data);
+}
 })
 .on("err",(err)=>{
     console.log(err);
 })
-.on("end",()=>{console.log(results,"result");});
+.on("end",()=>{console.log(HabitablePlanet,"result");});
